5c8e4366194aedf7b2ffcfba9dd38cc1a3459601
Henrique Caetano de Barros <henrique.barros@sc.senai.br>
2023-07-24 08:47:33 -0300

    organizing

--- script.js
+++ script.js
@@ -9,63 +9,98 @@
 
 function addElements() {
 
-    document.body.style.background = 'rgb(240,240,240)'
-    document.body.style.zIndex = 1
-
-    const header = createElement('div', document.body, 
-            'text-align: center; z-index: 2; font-size: 85px; font-weight: bold; margin-top: 30px')
+    const header = createElement('div', document.body, 'class', 'header')
     header.textContent = 'Etch-a-sketch'
 
-    const styleMain = 'margin:auto; display:flex; justify-content: space-between; ' +
-        'width: 70vw; height: 80vh; margin-top: 30px; z-index: 2'
-    const main = createElement('div', document.body, styleMain)
-
-    const styleButtons = 'display:flex; align-items:center; justify-content: space-between; flex-direction: column; ' +
-                            'width: 190px; height: 500px'    
-    const containerButtons = createElement('div', main, styleButtons)
-
-    const styleSquare = 'display:grid; background: #fff; min-width: 500px; height: 500px; ' +
-                       `box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)`     
-    const containerSquares = createElement('div', main, styleSquare)
+    const main = createElement('div', document.body, 'class', 'main')
     
-    const containerFake = createElement('div', main, styleButtons)
+    const containerButtons = createElement('div', main, 'class', 'containerButtons')
+     
+    const containerSquares = createElement('div', main, 'class', 'containerSquares')
+    
+    const containerFake = createElement('div', main, 'class', 'containerFake')
 
     createButtons(containerButtons)
 
     createToogle(containerButtons)
-}
+}   
 
 
-function createElement(tag, parent, style) {
+function createElement(tag, parent, attributeType, attributeName) {
     const element = document.createElement(tag)
-    element.setAttribute('style', style)
+    element.setAttribute(attributeType, attributeName)
     return parent.appendChild(element)
 }
 
 
 function createToogle(containerButtons) {
-    const header = createElement('h3', containerButtons, 'color: #000; text-align: center')
-    const toogle = createElement('input', containerButtons, '')
-            .setAttribute('type', 'range')
-            .setAttribute('min', 1)
-            .setAttribute('max', 64)
-            .setAttribute('value', 16)
-            .setAttribute('class', 'range')
+    const containerToogle = createElement('div', containerButtons, 'class', 'containerToogle')
+    const header = createElement('h3', containerToogle, 'class', 'toogleHeader')
+    const toogle = createElement('input', containerToogle, 'type', 'range')
+        toogle.setAttribute('min', 1)
+        toogle.setAttribute('max', 64)
+        toogle.setAttribute('value', 16)
+        toogle.setAttribute('class', 'range')
     
+    toogle.addEventListener('mousemove', changeRange)    
     header.textContent = toogle.value + ' X ' + toogle.value    
+}
+
+
+function changeRange(event) {
+
+    const inputRange = event.target
+
+    const toogleHeader = document.getElementsByClassName('toogleHeader')[0]    
+    toogleHeader.textContent = inputRange.value + ' X ' + inputRange.value
+
+
+    const containerSquares = document.getElementsByClassName('containerSquares')[0]
+    const color = document.getElementById('color')
+
+    let widthContainerSquares = containerSquares.clientWidth
+    const sideSquare = parseFloat(widthContainerSquares)/inputRange.value
+
+
+    for(let i = 0; i < (inputRange.value * inputRange.value); i++) {
+        const square = createElement('div', containerSquares, 'style', `width: ${sideSquare}; height: ${sideSquare}`)
+        square.addEventListener('mousedown', () => {
+            
+        })
+    }
+
 }
 
 
 function createButtons(containerButtons) {
     return [null, 'Color','Rainbow','Eraser','Clear'].map((b) => {
-
         if(b == null) {
-            return createElement('input', containerButtons, '').setAttribute('type', 'color')
-        }
-        const style = 'font-size: 16px; width: 190px; height: 40px; outline: none; ' +
-                'border: 1px solid #000; border-radius: 5px;  cursor:pointer' 
-        const button =  createElement('button', containerButtons, style)
+            const color =  createElement('input', containerButtons, 'type', 'color')
+            color.setAttribute('id', 'color')
+            return color
+        } 
+        const button =  createElement('button', containerButtons, 'class', b)
+        addEventButton(button, b)    
         return button.textContent = b
     })
 }
 
+
+function addEventButton(button, name) {
+
+    button.addEventListener('click', () => {
+        document.querySelectorAll('button').forEach(b => {
+            if(name != 'Clear' ) {
+                b.classList.remove('btnClicked')
+                button.classList.add('btnClicked')
+            }   
+        })
+        
+        if(name == 'Color') {
+            const color = document.getElementById('color')           
+            alert(color.value)
+        }
+
+
+    })
+}
