788969152e0b8df8c7fbed35ce3babb969895dda
Henrique Caetano de Barros <henrique.barros@sc.senai.br>
2023-07-29 21:07:25 -0300

    Finish

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-
 const options = {Color: true    , Rainbow: false, Eraser: false, Clear: false};
 let init = false;
 
-
 (function game() {
-
-    const header = createElement('div', document.body, 'class', 'header')
+    const pageContent = createElement('div', document.body, 'class', 'pageContent')
+    const header = createElement('div', pageContent, 'class', 'header')
     header.textContent = 'Etch-a-sketch'
 
-    const main = createElement('div', document.body, 'class', 'main')
-    
+    const main = createElement('div', pageContent, 'class', 'main')    
     const containerButtons = createElement('div', main, 'class', 'containerButtons')
-     
-    const containerSquares = createElement('div', main, 'class', 'containerSquares')
-    
-    const containerFake = createElement('div', main, 'class', 'containerFake')
+    createElement('div', main, 'class', 'containerSquares')
+    createElement('div', main, 'class', 'containerFake')
 
     createButtons(containerButtons)
-
     createToogle(containerButtons)
 }())
 
@@ -25,21 +19,17 @@ let init = false;
 function createElement(tag, parent, attributeType, attributeName) {
     const element = document.createElement(tag)
     element.setAttribute(attributeType, attributeName)
-    if(parent != null) {
-        parent.appendChild(element)
-    }
-    return element
+    return parent.appendChild(element)
 }
 
 
 function createToogle(containerButtons) {
     const containerToogle = createElement('div', containerButtons, 'class', 'containerToogle')
-    const header = createElement('h3', containerToogle, 'class', 'toogleHeader')
+    const header = createElement('div', containerToogle, 'class', 'toogleHeader')
     const toogle = createElement('input', containerToogle, 'type', 'range')
         toogle.setAttribute('min', 1)
         toogle.setAttribute('max', 64)
         toogle.setAttribute('value', 16)
-        toogle.setAttribute('class', 'range')
 
     addSquares(toogle)    
     toogle.addEventListener('mouseup', (e) => addSquares(e.target))    
@@ -64,41 +54,41 @@ function addSquares(inputRange) {
     }
 
     containerSquares.style.gridTemplateColumns = `repeat(auto-fill, minmax(${sideSquare}px, 1fr))`
-
-    if(init == false) {
-        init = true
-        paint()
-    }        
-
+    
+    if(!init) paint(containerSquares)
 }
 
 
-function paint() {
+function paint(containerSquares) {
     
-    const containerSquares = document.getElementsByClassName('containerSquares')[0]
-
-    containerSquares.addEventListener('mousedown', (e) => {
-        
+    init = true
+    
+    containerSquares.addEventListener('mousedown', () => {
         [...containerSquares.children].map(square => {
-            square.addEventListener('mouseover', () => {
-
-                if(options['Color'] == true) {
-                    const color = document.getElementById('color')
-                    square.style.background = color.value
-                }
-                if(options['Rainbow'] == true) {
-                    const color1 = Math.floor(Math.random() * 257)
-                    const color2 = Math.floor(Math.random() * 257)
-                    const color3 = Math.floor(Math.random() * 257)
-                    square.style.background = `rgb(${color1},${color2},${color3})`
-                }
-                if(options['Eraser'] == true) {
-                    square.style.background = '#fff'
-                }
-    
-            })
+            square.addEventListener('mouseover', paintSquare)
         })
-    })  
+    })
+    containerSquares.addEventListener('mouseup', () => {
+        [...containerSquares.children].map(square => {
+            square.removeEventListener('mouseover', paintSquare)
+        })
+    })
+
+    function paintSquare(e) {
+        if(options['Color']) {
+            const color = document.getElementById('color')
+            e.target.style.background = color.value
+        }
+        if(options['Rainbow']) {
+            const color1 = Math.floor(Math.random() * 257)
+            const color2 = Math.floor(Math.random() * 257)
+            const color3 = Math.floor(Math.random() * 257)
+            e.target.style.background = `rgb(${color1},${color2},${color3})`
+        }
+        if(options['Eraser']) {
+            e.target.style.background = '#fff'
+        }
+    }
 }
 
 
@@ -120,9 +110,7 @@ function createButtons(containerButtons) {
 
 
 function addEventButton(button, name) {
-
-    button.addEventListener('click', () => {
-        
+    button.addEventListener('click', () => {       
         if(name == 'Clear') {
             document.querySelectorAll('.square').forEach(s => s.style.background = '#fff') 
             return
